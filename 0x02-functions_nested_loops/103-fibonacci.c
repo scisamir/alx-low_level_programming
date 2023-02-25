6b668bde4283bdca1ec30d117a22111767aee272
Samir Idris <samzidris11@gmail.com>
2023-02-25 07:15:09 -0800

    fix correct output

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,18 +9,19 @@
 
 int main(void)
 {
-	long int a = 1, b = 2, c, sum = 3;
+	int a = 1, b = 2, c, sum = b;
 
 	while (c <= 4000000)
 	{
 		c = a + b;
 
-		sum += c;
+		if (c % 2 ==0)
+			sum = sum + c;
 
 		a = b;
 		b = c;
 	}
-	printf("%ld\n", sum);
+	printf("%d\n", sum);
 
 	return (0);
 }
