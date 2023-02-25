7d92f7a73600a8365d077cf8770d08703daa96a5
Samir Idris <samzidris11@gmail.com>
2023-02-25 00:20:01 -0800

    made it work right

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,13 +16,13 @@ void print_triangle(int size)
 
 	else
 	{
-		for (i = 0; i < size; i++)
-			for (j = 0; j < size - i; j++)
+		for (i = 0; i < n; i++)
+		{
+			for (j = 1; j < n - i; j++)
 				_putchar(' ');
 			for (k = 0; k <= i; k++)
 				_putchar(35);
 			_putchar('\n');
-		_putchar('\n');
+		}
 	}
-	
 }
