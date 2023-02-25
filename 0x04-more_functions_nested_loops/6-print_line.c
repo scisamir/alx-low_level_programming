55e4991283a835719ea461f4eb4b94a77f5eadb2
Samir Idris <samzidris11@gmail.com>
2023-02-24 23:11:47 -0800

    fix break statement declaration outside loop

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,13 +12,12 @@ void print_line(int n)
 	int i;
 
 	if (n <= 0)
-		break;
+		_putchar('\n');
 
 	else
 	{
 		for (i = 0; i < n; i++)
 			_putchar(95);
+		_putchar('\n');
 	}
-
-	_putchar('\n');
 }
