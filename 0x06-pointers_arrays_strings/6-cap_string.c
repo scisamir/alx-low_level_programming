29b1e540fef208a165259b3127d289c7203f4883
Samir Idris <samzidris11@gmail.com>
2023-03-02 12:15:20 -0800

    fix trailing whitespace

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,23 +9,23 @@
 
 char *cap_string(char *str)
 {
-        int i, j, k;
-        char seps[14] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\0'};
+	int i, j, k;
+	char seps[14] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\0'};
 
-        for (i = 0; str[i] != '\0'; i++)
-        {
-                for (j = 'a'; j <= 'z'; j++)
-                {
-                        if (str[i] == j)
-                        {
-                                for (k = 0; k <= 12; k++)
-                                {
-                                        if (str[i - 1] == seps[k])
-                                                str[i] = j - 32;
-                                }
-                        }
-                }
-        }
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			if (str[i] == j)
+			{
+				for (k = 0; k <= 12; k++)
+				{
+					if (str[i - 1] == seps[k])
+						str[i] = j - 32;
+				}
+			}
+		}
+	}
 
-        return (str);
+	return (str);
 }
