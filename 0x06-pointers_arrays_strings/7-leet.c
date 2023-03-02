cac127dd886f7ca8c9c9b8e41839bce26cc6f7e0
Samir Idris <samzidris11@gmail.com>
2023-03-02 06:43:08 -0800

    fix code indent

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,19 +9,19 @@
 
 char *leet(char *str)
 {
-        int i = 0, j;
-        char lets[6] = {'a', 'e', 'o', 't', 'l', '\0'};
-        char nums[6] = {'4', '3', '0', '7', '1', '\0'};
+	int i = 0, j;
+	char lets[6] = {'a', 'e', 'o', 't', 'l', '\0'};
+	char nums[6] = {'4', '3', '0', '7', '1', '\0'};
 
-        while (str[i] != '\0')
-        {
-                for (j = 0; j < 5; j++)
-                {
-                        if (str[i] == lets[j] || str[i] == (lets[j] - 32))
-                                str[i] = nums[j];
-                }
-                i++;
-        }
+	while (str[i] != '\0')
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (str[i] == lets[j] || str[i] == (lets[j] - 32))
+				str[i] = nums[j];
+		}
+		i++;
+	}
 
-        return (str);
+	return (str);
 }
