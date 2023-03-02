c30638cc32f2a9dc7c10ebef9e2a61cf32d0ca1d
Samir Idris <samzidris11@gmail.com>
2023-03-02 11:32:50 +0100

    modify code

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,13 +12,9 @@
 char *_strncat(char *dest, char *src, int n)
 {
         int i = 0, j = 0, k = 0, dest_len, src_len, new_len;
-        char *new_arr;
 
         while (dest[i] != '\0')
-        {
-                new_arr[i] = dest[i];
                 i++;
-        }
 
 
         while (src[j] != '\0')
@@ -36,7 +32,7 @@ char *_strncat(char *dest, char *src, int n)
                 if (k > dest_len)
                         dest[k] = src[k - src_len + 1];
                 else
-                        dest[k] = new_arr[k];
+                        dest[k] = dest[k];
 
                 k++;
         }
