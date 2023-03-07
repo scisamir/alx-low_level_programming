7e5598ccf8b8efd899a53685b3ccf8498770f518
Samir Idris <samzidris11@gmail.com>
2023-03-07 01:20:12 -0800

    complete _atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,40 +10,26 @@
 
 int _atoi(char *s)
 {
-	// printf("Before variable declarations");
+	int i, sign = 1, sum = 0, indicator = 0;
 
-	int i = 0, j = 0, num_count = 0;
-	int sign = 1;
-	char num[36];
-
-	// printf("Just before loop for the input parameter");
-
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		// printf("Just inside first loop");
+		if (s[i] == '+')
+			sign *= +1;
+		else if (s[i] == '-')
+			sign *= -1;
 
-		if (s[i] == '-' || s[i] == '+')
-			sign * s[i];
-		if (s[i] >= '0' && s[i] <= '9')
+		if ((s[i] >= '0') && (s[i] <= '9'))
 		{
-			*(num + num_count) = s[i];
-			num_count++;
-
-			int tr;
-			tr = i + 1;
-			if (*(s + tr) >= '0' && *(s + tr) <= '9')
-				break;
+			indicator = 1;
+			sum = sum * 10 + s[i] - '0';
 		}
-
-		i++;
+		else if (indicator)
+			break;
 	}
 
-	while (num[j] != '\0')
-	{
-		if (j == 0)
-			_putchar(sign * num[j]);
-		else
-			_putchar(num[j]);
-		j++;
-	}
+	if (sign < 0)
+		sum *= -1;
+
+	return (sum);
 }
