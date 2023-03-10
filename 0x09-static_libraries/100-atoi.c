ddb089dffd824bb35f948c29eb7157a60b51d229
Samir Idris <samzidris11@gmail.com>
2023-03-10 05:52:09 -0800

    fix 100-atoi in the static library

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
+ *
+ * Return: the converted string or 0 if no numbers in the string
+ */
+
+int _atoi(char *s)
+{
+}
