2c8153044187e76984079dd3ebfe10340b374abc
Samir Idris <samzidris11@gmail.com>
2023-03-10 11:00:27 -0800

    update the static library, libmy.a

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -10,4 +10,27 @@
 
 int _atoi(char *s)
 {
+	int i, sign = 1, indicator = 0;
+	unsigned int sum = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '+')
+			sign *= +1;
+		else if (s[i] == '-')
+			sign *= -1;
+
+		if ((s[i] >= '0') && (s[i] <= '9'))
+		{
+			indicator = 1;
+			sum = sum * 10 + s[i] - '0';
+		}
+		else if (indicator)
+			break;
+	}
+
+	if (sign < 0)
+		sum *= -1;
+
+	return (sum);
 }
