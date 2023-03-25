c5f7891b78b4d31bc6c9122d2a80980cf3e5c9b0
Samir Idris <samzidris11@gmail.com>
2023-03-25 14:14:47 -0700

    fix errors

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
-		exit(EXIT_SUCCESS);
+		return;
 	else
 	{
 		if ((*d).name == NULL)
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -10,8 +10,10 @@
 
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->age);
-	free(d->owner);
-	free(d);
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
