a7b88884e357646b06996b60fe1c3749ade39441
Samir Idris <samzidris11@gmail.com>
2023-03-25 03:49:40 -0700

    add readme and create dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+## ALX C structs, typedefs
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef __DOG__
+#define __DOG__
+
+/**
+ * struct dog - a structure describing a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* __DOG__ */
