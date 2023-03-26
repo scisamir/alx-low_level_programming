b095b1b9739685ad8df5b3afe8b1d57a24b78b3e
Samir Idris <samzidris11@gmail.com>
2023-03-25 22:17:45 -0700

    make function to save copy of dog name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,15 +12,42 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i = 0;
 	dog_t *my_dog;
 
 	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
 		return (NULL);
 
-	my_dog->name = name;
+	while (name[i] != '\0')
+		i++;
+	i += 1;
+
+	my_dog->name = malloc(sizeof(char) * i);
+	if (my_dog->name == NULL)
+	{
+		free(my_dog);
+		return (NULL);
+	}
+	else
+		my_dog->name = name;
+
 	my_dog->age = age;
-	my_dog->owner = owner;
+
+	i = 0;
+	while (owner[i] != '\0')
+		i++;
+	i += 1;
+
+	my_dog->owner = malloc(sizeof(char) * i);
+	if (my_dog->owner == NULL)
+	{
+		free(my_dog->name);
+		free(my_dog);
+		return (NULL);
+	}
+	else
+		my_dog->owner = owner;
 
 	return (my_dog);
 }
