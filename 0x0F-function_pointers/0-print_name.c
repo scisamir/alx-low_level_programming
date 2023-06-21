3543f452d7a2bac500b713f429e1945be1a047bd
Samir Idris <samzidris11@gmail.com>
2023-06-21 11:22:52 +0100

    complete function pointer tasks

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: the name to be printed
+ * @f: the function to print the name
+ *
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		(*f)(name);
+}
