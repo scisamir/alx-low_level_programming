03419363737a03eb53f42369798853c29d3a3fbb
Samir Idris <samzidris11@gmail.com>
2023-07-08 13:41:29 -0700

    update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,7 +9,7 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 536870912; /* 100000000000000000000000000000 */
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
 	unsigned int check = 0;
 
 	if (n == 0)
