6a06fc994e79ef9ab207de5328df666debe7251d
Samir Idris <samzidris11@gmail.com>
2023-09-08 23:13:46 +0100

    update tasks 3 and 4 of C - hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,12 +12,25 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
-	hash_node_t *current = NULL;
+	hash_node_t *current = NULL, *ptr = NULL;
 
-	if (!ht || !key || !value)
+	if (!ht || !key || *key == '\0' || !value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+	ptr = ht->array[index];
+
+	while (ptr)
+	{
+		if (!strcmp(ptr->value, value))
+		{
+			free(ptr->value);
+			ptr->value = strdup(value);
+			return (1);
+		}
+
+		ptr = ptr->next;
+	}
 
 	current = malloc(sizeof(hash_node_t));
 	if (current == NULL)
