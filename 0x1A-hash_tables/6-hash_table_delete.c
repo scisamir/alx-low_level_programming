640f2e90563a24b9dce835cdb5f1d001990cbb2b
Samir Idris <samzidris11@gmail.com>
2023-09-09 00:10:37 +0100

    >>> del ht

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: the hash table
+ *
+ * Return: Nothing
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *curr = NULL, *temp = NULL;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		curr = ht->array[i];
+
+		while (curr)
+		{
+			free(curr->key);
+			free(curr->value);
+
+			temp = curr;
+			curr = curr->next;
+
+			free(temp);
+		}
+	}
+
+	free(ht);
+}
