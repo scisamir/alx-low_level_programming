d09a4c09fd11369ec2314932ae187c2ccce3cdf9
Samir Idris <samzidris11@gmail.com>
2023-09-08 23:53:45 +0100

    >>> print(ht)

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -13,10 +13,7 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *curr = NULL, *ptr = NULL;
 
 	if (!ht)
-	{
-		printf("{}\n");
 		return;
-	}
 
 	printf("{");
 	for (i = 0; i < ht->size; i++)
