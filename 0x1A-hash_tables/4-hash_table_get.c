18c34799d87c98325aed720039f860da46b0c757
Samir Idris <samzidris11@gmail.com>
2023-09-09 00:28:02 +0100

    update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,11 +12,22 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
+	hash_node_t *ptr = NULL;
+
+	if (!ht || !key || *key == '\0')
+		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
-	if (ht->array[index] == NULL)
-		return (NULL);
+	ptr = ht->array[index];
 
-	return ((ht->array[index])->value);
+	while (ptr)
+	{
+		if (!strcmp(ptr->key, key))
+			return (ptr->value);
+
+		ptr = ptr->next;
+	}
+
+	return (NULL);
 }
