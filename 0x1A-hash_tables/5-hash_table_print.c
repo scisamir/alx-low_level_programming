eb7eb22379b4705ee582c24c4c097335c0b4e058
Samir Idris <samzidris11@gmail.com>
2023-09-08 23:47:26 +0100

    >>> print(ht)

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,43 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: the hash table
+ *
+ * Return: Nothing
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, sign = 0;
+	hash_node_t *curr = NULL, *ptr = NULL;
+
+	if (!ht)
+	{
+		printf("{}\n");
+		return;
+	}
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		curr = ht->array[i];
+		if (curr)
+		{
+			if (sign++ == 0)
+				printf("'%s': '%s'", curr->key, curr->value);
+			else
+				printf(", '%s': '%s'", curr->key, curr->value);
+
+			if (curr->next)
+				ptr = curr->next;
+
+			while (ptr)
+			{
+				printf(", '%s': '%s'", ptr->key, ptr->value);
+				ptr = ptr->next;
+			}
+		}
+	}
+	printf("}\n");
+}
