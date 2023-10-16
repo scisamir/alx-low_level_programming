cc671ea54126ce833098a8e204f28b1b861d931f
Samir Idris <samzidris11@gmail.com>
2023-10-16 07:19:18 +0100

    Delete at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -14,12 +14,11 @@ int delete_at_beginning(dlistint_t **head)
 
 	if (ptr != NULL)
 	{
-		if ((*head)->next != NULL)
-		{
-			*head = (*head)->next;
+		*head = (*head)->next;
+		if ((*head) != NULL)
 			(*head)->prev = NULL;
-		}
 
+		ptr->next = NULL;
 		free(ptr);
 		ptr = NULL;
 
@@ -49,7 +48,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		return (-1);
 
 	if (index == 0)
-		return(delete_at_beginning(head));
+		return (delete_at_beginning(head));
 
 	while (ptr->next != NULL)
 	{
@@ -58,22 +57,15 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			temp = ptr->next;
 			next_node = temp->next;
 			ptr->next = next_node;
-			next_node->prev = ptr;
+			if (next_node)
+				next_node->prev = ptr;
+			temp->prev = NULL, temp->next = NULL;
 			free(temp);
 			temp = NULL;
 			return (1);
 		}
 		ptr = ptr->next;
 		pos++;
-	}
-
-	if (pos == index && pos > 1)
-	{
-		ptr->next = NULL;
-		ptr->prev->next = NULL;
-		free(ptr);
-		ptr = NULL;
-		return (1);
 	}
 
 	return (-1);
