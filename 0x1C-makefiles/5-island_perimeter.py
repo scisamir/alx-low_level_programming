2bf45bd8b1ba49ddcaf0b5127ee5c972758c11e6
Samir Idris <samzidris11@gmail.com>
2023-10-26 09:39:14 +0100

    Technical interview prep: Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,21 +8,22 @@ def island_perimeter(grid):
     """ Returns the perimeter of the island described in grid """
     height = 0
     width = 0
-    second_width = 0
 
-    for row in grid:
+    width_pos_list = []
+
+    grid_height = len(grid)
+    grid_width = len(grid[0]) if grid_height else 0
+
+    for i in range(grid_height):
         height_check = False
 
-        for cell in row:
-            if cell == 1:
-                second_width += 1
+        for j in range(grid_width):
+            if grid[i][j] == 1 and j not in width_pos_list:
+                width += 1
+                width_pos_list.append(j)
 
-            if cell == 1 and height_check is False:
+            if grid[i][j] == 1 and height_check is False:
                 height_check = True
                 height += 1
-
-        if second_width > width:
-            width = second_width
-        second_width = 0
 
     return (2 * (height + width))
